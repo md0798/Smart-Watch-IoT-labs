d42c3f5196c6ce055225ff9fd28d1ce57c342127
md0798 <81832050+md0798@users.noreply.github.com>
2021-05-05 19:05:01 -0400

    Update Lab3_Meet_check1.py

--- Lab_3/Lab3_Meet_check1.py
+++ Lab_3/Lab3_Meet_check1.py
@@ -8,7 +8,7 @@ import ssd1306
 i2c = machine.I2C(-1, machine.Pin(5), machine.Pin(4))#i2c for oled
 oled = ssd1306.SSD1306_I2C(128, 32, i2c)
 
-url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCyLBmfp5Ghrplz5RH9JN9NR296fN-Nl9c" #setting up url with api
+url = "https://www.googleapis.com/geolocation/v1/geolocate?key=<insert api key here>" #setting up url with api
 
 
 r = urequests.post(url,data=json.dumps({})) # requesting location data from google api
